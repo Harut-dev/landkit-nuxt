78d765c044b8dcff46c9b5dcb46fc3e89cd602ae
Harut <harutyungyulasaryan.dev@gmail.com>
2020-09-18 20:05:08 +0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TheHeader/>
+    <Nuxt />
+    <TheFooter/>
+  </div>
+</template>
+
+<script>
+import TheHeader from "../components/header/TheHeader";
+import TheFooter from "../components/footer/TheFooter";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    TheFooter
+  },
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'landkit',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    {src: '@/assets/css/theme.css'},
+    {src: "flickity/dist/flickity.css" },
+    // flickity/dist/flickity.min.css"
+    // SCSS file in the project
+    // '@/assets/scss/theme.scss'
+  ],
+  render: {
+    bundleRenderer: {
+      shouldPreload: (file, type) => {
+        return ['script', 'style', 'font'].includes(type)
+      }
+    }
+  },
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    { src: '~/plugins/VueFlickity.js', ssr: false }
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
